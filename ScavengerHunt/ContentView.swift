8a522218dfe41340b48272057c557507b37e5c63
Russell Gordon <rgordon@lcs.on.ca>
2024-09-30 08:14:14 -0400

    Initial Commit

---
+++ ScavengerHunt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScavengerHunt
+//
+//  Created by Russell Gordon on 2024-09-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScavengerHunt/ScavengerHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScavengerHuntApp.swift
+//  ScavengerHunt
+//
+//  Created by Russell Gordon on 2024-09-30.
+//
+
+import SwiftUI
+
+@main
+struct ScavengerHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
